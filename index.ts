aa30e57dd3f5528bb7446bf7fe9702bbf5f35c84
Youn Junseok <y2o2u2n@gmail.com>
2019-02-07 17:41:21 +0900

    #3 First steps with Typescript

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("hello");
+const name = "y2o2u2n",
+    age = 24,
+    gender = "male";
+
+const sayHi = (name, age, gender?) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+
+sayHi(name, age);
+
+export {};
