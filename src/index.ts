db296be54b3b1b37d7fa1fa3e425b204e474266f
Youn Junseok <y2o2u2n@gmail.com>
2019-02-07 18:12:22 +0900

    #6 Classes on Typescript part One

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,20 @@
-interface Human {
-    name: string,
-    age: number,
-    gender: string
+class Human {
+    public name: string;
+    public age: number;
+    public gender: string;
+    constructor(name: string, age:number, gender:string) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
 }
 
-const person = {
-    name: "y2o2u2n",
-    age: 22,
-    gender: "male"
-}
+const y2o2u2n = new Human("y2o2u2n", 22, "male");
 
 const sayHi = (person: Human): string => {
     return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
 };
 
-console.log(sayHi(person));
+console.log(sayHi(y2o2u2n));
 
 export { };
