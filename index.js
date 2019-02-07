1ff6cd642d6f61384c9eb9e723646042f9efa40f
Youn Junseok <y2o2u2n@gmail.com>
2019-02-07 18:02:00 +0900

    #4 Types in Typescript

--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const name = "y2o2u2n", age = 24, gender = "male";
-const sayHi = (name, age, gender) => {
-    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
-};
-sayHi(name, age);
-//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+const sayHi = (name:String, age:number, gender:String): string => {
+    return `Hello ${name}, you are ${age}, you are a ${gender}!`;
+};
+
+console.log(sayHi("y2o2u2n", 24, "male"));
+
+export {};
